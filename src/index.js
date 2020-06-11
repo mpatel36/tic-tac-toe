35f8f8fae9f6e88db6d8b712df7b9423b9e3842b
Patel <mp185344@ncr.com>
2020-06-11 14:11:44 -0400

    fixed Readme

---
+++ src/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button 
+          className="square" 
+          onClick={() => this.setState({value: 'X'}) 
+          /*setState from an onclick handler in the rendor
+          method tells React to rerender Square when button 
+          clicked. 
+          When you call set-state in a component
+          REact automatically updates the child components
+          inside of it too.
+          Currently each Square compo maintains games state.
+          To check for winner maintain the value of each of the
+          9 squares in one location.*/}
+      >
+        {this.state.value 
+        /*X will appear on board.
+        Now numbers do not appear because the state value is
+        being taken, not props. Board's renderSquare currently
+        sends props to Square, but they are ignored*/}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  renderSquare(i) {
+    return <Square value={i}/>;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+  
