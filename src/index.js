604bb22d8fc84beb05123562942fb3cafdea7de8
Patel <mp185344@ncr.com>
2020-06-13 23:34:50 -0400

    Display the location for each move in the format (col, row) in the move history list.

--- src/index.js
+++ src/index.js
@@ -1,56 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-/* Complete Tic-tac toe:
 
-  TODO:
-    learn more about shouldComponentUpdate() and 
-    how you can build 'pure components'
 
-  COOL:
-    on Compon. keys:
-    -- 'key' is a special and reserved property in React 
-      (along with 'ref', a more advanced feature). When an 
-      element is created, React extracts the key property and
-      stores the key directly on the returned element. --
-    -- Even though key may look like it belongs in props, 
-      key cannot be referenced using this.props.key. --
-    -- React automatically uses key to decide which 
-      components to update. --
-    -- A component cannot inquire about its key. --
-    -- it is strongly recommended that you assign proper keys
-      whenever building dynamic lists since React is a 
-      computer program and doesn't know how to arrange items 
-      in the list as we intended --
-    -- we need to specify key property for each list item to
-      differentiate each list item from its siblings --
-    -- If no key is specified, React will present a warning 
-      and use the array index as a key by default.
-
-    unlike array push() method, concat() doesn't
-    mutate original array
-
-    in JS, arrays have a map() method that is used for 
-    mapping data to other data, for example
-        const numbers = [1, 2, 3];
-        const doubled = numbers.map(x => x * 2); // [2, 4, 6]
-    
-    in React elements are first-class JS objects --
-    -- we can pass them around in our applications --
-    -- we can use an array of React elements to render
-      multiple items
-
-    in JS, all functions are first-class objects. Meaning:
-    -- A function is an instance of the Object type --
-    -- A function can have properties and has a link 
-      back to its constructor method --
-    -- You can store the function in a variable --
-    -- You can pass the function as a parameter 
-      to another function --
-    -- You can return the function from a function
-*/
-/*
-*/
+function calculateGridLocation(i) {
+  let col = i%3 ;
+  let row = Math.floor(i/3);
+  return [col, row];
+}
 
 function Square(props) {
   return (
@@ -60,10 +17,11 @@ function Square(props) {
     >
       {props.value}
     </button>
-  )
+  );
 }
-
+ 
 class Board extends React.Component {
+
   renderSquare(i) {
     return (
       <Square 
@@ -72,9 +30,10 @@ class Board extends React.Component {
       />
     );
   }
+
   render() {
     return (
-      <div>
+      <div> 
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -102,13 +61,16 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
-      stepNumber: 0, /*2.*/
+      moveLocations: Array(9).fill(null),
+      stepNumber: 0, 
       xIsNext: true,
     };
   }
 
   handleClick(i) {
     const history = this.state.history.slice(0, 
+      this.state.stepNumber + 1);
+    const moveLocations = this.state.moveLocations.slice(0,
       this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
@@ -120,24 +82,30 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      moveLocations: moveLocations.concat([
+        calculateGridLocation(i)
+      ]),
       stepNumber: history.length, 
       xIsNext: !this.state.xIsNext,
     }); 
   }
+
   jumpTo(step) { 
     this.setState({
       stepNumber: step, 
       xIsNext: (step % 2) === 0,
     });
   }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber]; 
+    const moveLocations = this.state.moveLocations;
     const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
+    const moves = history.map((boardState, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move + ': located at ' 
+          + '(' + moveLocations[move] +')' :
         'Go to game start';
       return (
         <li key={move}>
@@ -198,4 +166,4 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-  
+   
